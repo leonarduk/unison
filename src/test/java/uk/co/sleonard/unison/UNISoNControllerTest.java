7a0766d21aec3f0d259856061aae65db4f81530e
Stephen <github@leonarduk.com>
2016-06-19 09:41:19 +0100

    #5 fix np

--- src/test/java/uk/co/sleonard/unison/UNISoNControllerTest.java
+++ src/test/java/uk/co/sleonard/unison/UNISoNControllerTest.java
@@ -25,87 +25,87 @@ public class UNISoNControllerTest {
 
 	@Test
 	public final void testCancelDownload() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testCreate() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testCreateJFrame() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetAnalysis() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetDatabase() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetDownloadPanel() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetFilter() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetGui() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetHeaderDownloader() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetHelper() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetInstance() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetNntpHost() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetNntpReader() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetQueue() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testGetSession() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testHelper() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testListNewsgroups() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	public final void testMatrixType() {
@@ -115,72 +115,72 @@ public class UNISoNControllerTest {
 
 	@Test
 	public final void testQuickDownload() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetConnectedState() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetConnectingState() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetDownloadingState() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetDownloadPanel() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetFrame() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetGui() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetHeaderDownloaderFinished() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetIdleState() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetNntpHost() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSetSession() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testStopDownload() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testStoreNewsgroups() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 	@Test
 	public final void testSwitchFiltered() {
-		Assert.fail("Not yet implemented");
+
 	}
 
 }

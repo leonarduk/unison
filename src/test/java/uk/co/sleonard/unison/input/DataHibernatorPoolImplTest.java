f7dec3c971b736a746e61382ecaa690d7e41b5bb
Steve <leonarduk@users.noreply.github.com>
2025-08-16 20:09:27 +0100

    Add tests for downloader and hibernator pool

---
+++ src/test/java/uk/co/sleonard/unison/input/DataHibernatorPoolImplTest.java
@@ -0,0 +1,30 @@
+package uk.co.sleonard.unison.input;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ * Tests for {@link DataHibernatorPoolImpl}.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DataHibernatorWorker.class)
+public class DataHibernatorPoolImplTest {
+
+    /**
+     * Verifies stopAllDownloads delegates to DataHibernatorWorker.stopDownload().
+     */
+    @Test
+    public void testStopAllDownloadsDelegates() {
+        PowerMockito.mockStatic(DataHibernatorWorker.class);
+
+        DataHibernatorPool pool = new DataHibernatorPoolImpl();
+        pool.stopAllDownloads();
+
+        PowerMockito.verifyStatic(DataHibernatorWorker.class, Mockito.times(1));
+        DataHibernatorWorker.stopDownload();
+    }
+}
---
+++ src/test/java/uk/co/sleonard/unison/utils/DownloaderImplTest.java
@@ -0,0 +1,50 @@
+package uk.co.sleonard.unison.utils;
+
+import org.hibernate.Session;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import uk.co.sleonard.unison.UNISoNException;
+import uk.co.sleonard.unison.datahandling.HibernateHelper;
+import uk.co.sleonard.unison.datahandling.DAO.DownloadRequest.DownloadMode;
+import uk.co.sleonard.unison.input.FullDownloadWorker;
+import uk.co.sleonard.unison.input.NewsArticle;
+import uk.co.sleonard.unison.input.NewsClient;
+import uk.co.sleonard.unison.input.NewsGroupReader;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Tests for {@link DownloaderImpl}.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(FullDownloadWorker.class)
+public class DownloaderImplTest {
+
+    /**
+     * Ensures that addDownloadRequest delegates to FullDownloadWorker with correct parameters.
+     */
+    @Test
+    public void testAddDownloadRequestDelegatesCorrectly() throws UNISoNException {
+        String nntpHost = "host";
+        LinkedBlockingQueue<NewsArticle> queue = new LinkedBlockingQueue<>();
+        NewsClient newsClient = Mockito.mock(NewsClient.class);
+        NewsGroupReader reader = Mockito.mock(NewsGroupReader.class);
+        HibernateHelper helper = Mockito.mock(HibernateHelper.class);
+        Session session = Mockito.mock(Session.class);
+
+        PowerMockito.mockStatic(FullDownloadWorker.class);
+
+        DownloaderImpl downloader = new DownloaderImpl(nntpHost, queue, newsClient, reader, helper, session);
+        String usenetId = "<123>";
+        DownloadMode mode = DownloadMode.ALL;
+
+        downloader.addDownloadRequest(usenetId, mode);
+
+        PowerMockito.verifyStatic(FullDownloadWorker.class, Mockito.times(1));
+        FullDownloadWorker.addDownloadRequest(usenetId, mode, nntpHost, queue, newsClient, reader, helper, session);
+    }
+}
